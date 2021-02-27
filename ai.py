475e3179b92c2d67d5cd702add8e9e24fb3ae8e6
Mixanio <75267478+Mixanio@users.noreply.github.com>
2021-02-27 15:34:16 +0500

    Update ai.py

--- ai.py
+++ ai.py
@@ -19,7 +19,40 @@ def ai(pole, params): # 1 - ai, 2 - player
                     break        
         elif params == 1:
             for i in pole: # по горизонтали
-                if (sum(i) == 4 or (sum(i) == 2 and 2 not in i)) and 0 in i:
+                if sum(i) == 2 and 2 not in i:
+                    for j in i:
+                        if j == 0:
+                            if 0 in i:
+                                pole[pole.index(i)][i.index(j)] = 1
+                                return pole
+            if turn: # по вертикали
+                for i in range(0, 3): 
+                    x = []
+                    for j in range(0, 3):
+                        x.append(pole[j][i])
+                    if sum(x) == 2 and 2 not in x:
+                        if 0 in x:
+                            pole[x.index(0)][i] = 1
+                            return pole
+            if turn: # по диагонали 1
+                x = []
+                for i in range(0, 3):
+                    x.append(pole[i][i])
+                if sum(x) == 2 and 2 not in x:
+                    if pole[x.index(0)][x.index(0)] == 0:
+                        pole[x.index(0)][x.index(0)] = 1
+                        return pole
+            if turn: # по диагонали 2
+                x = []
+                for i in range(0, 3):
+                    x.append(pole[i][2 - i])
+                if sum(x) == 2 and 2 not in x:
+                    if pole[x.index(0)][2 - x.index(0)] == 0:
+                        pole[x.index(0)][2 - x.index(0)] = 1
+                        return pole
+                        
+            for i in pole: # по горизонтали
+                if sum(i) == 4 and 0 in i:
                     for j in i:
                         if j == 0:
                             if 0 in i:
@@ -31,7 +64,7 @@ def ai(pole, params): # 1 - ai, 2 - player
                     x = []
                     for j in range(0, 3):
                         x.append(pole[j][i])
-                    if (sum(x) == 4 or (sum(x) == 2 and 2 not in x)) and 0 in x:
+                    if sum(x) == 4 and 0 in x:
                         if 0 in x:
                             pole[x.index(0)][i] = 1
                             turn = False
@@ -40,7 +73,7 @@ def ai(pole, params): # 1 - ai, 2 - player
                 x = []
                 for i in range(0, 3):
                     x.append(pole[i][i])
-                if (sum(x) == 4 or (sum(x) == 2 and 2 not in x)) and 0 in x:
+                if sum(x) == 4 and 0 in x:
                     if pole[x.index(0)][x.index(0)] == 0:
                         pole[x.index(0)][x.index(0)] = 1
                         turn = False
@@ -48,7 +81,7 @@ def ai(pole, params): # 1 - ai, 2 - player
                 x = []
                 for i in range(0, 3):
                     x.append(pole[i][2 - i])
-                if (sum(x) == 4 or (sum(x) == 2 and 2 not in x)) and 0 in x:
+                if sum(x) == 4 and 0 in x:
                     if pole[x.index(0)][2 - x.index(0)] == 0:
                         pole[x.index(0)][2 - x.index(0)] = 1
                         turn = False
@@ -89,5 +122,3 @@ def check_win(pole):
         if 0 not in line and sum(line) % 3 == 0:
             return sum(line) // 3     
     return 0
-
-
