9269b605cc3efb967b8ee7ba47a59b9322dc4b3d
Mixanio <75267478+Mixanio@users.noreply.github.com>
2021-02-28 15:14:03 +0500

    Update ai.py

--- ai.py
+++ ai.py
@@ -97,7 +97,84 @@ def ai(pole, params): # 1 - ai, 2 - player
                         pole[y][x] = 1
                         break            
         elif params == 2:
-            pass
+            for i in pole: # по горизонтали
+                if sum(i) == 2 and 2 not in i:
+                    for j in i:
+                        if j == 0:
+                            if 0 in i:
+                                pole[pole.index(i)][i.index(j)] = 1
+                                return pole
+            if turn: # по вертикали
+                for i in range(0, 3): 
+                    x = []
+                    for j in range(0, 3):
+                        x.append(pole[j][i])
+                    if sum(x) == 2 and 2 not in x:
+                        if 0 in x:
+                            pole[x.index(0)][i] = 1
+                            return pole
+            if turn: # по диагонали 1
+                x = []
+                for i in range(0, 3):
+                    x.append(pole[i][i])
+                if sum(x) == 2 and 2 not in x:
+                    if pole[x.index(0)][x.index(0)] == 0:
+                        pole[x.index(0)][x.index(0)] = 1
+                        return pole
+            if turn: # по диагонали 2
+                x = []
+                for i in range(0, 3):
+                    x.append(pole[i][2 - i])
+                if sum(x) == 2 and 2 not in x:
+                    if pole[x.index(0)][2 - x.index(0)] == 0:
+                        pole[x.index(0)][2 - x.index(0)] = 1
+                        return pole
+                        
+            for i in pole: # по горизонтали
+                if sum(i) == 4 and 0 in i:
+                    for j in i:
+                        if j == 0:
+                            if 0 in i:
+                                pole[pole.index(i)][i.index(j)] = 1
+                                turn = False
+                                break
+            if turn: # по вертикали
+                for i in range(0, 3): 
+                    x = []
+                    for j in range(0, 3):
+                        x.append(pole[j][i])
+                    if sum(x) == 4 and 0 in x:
+                        if 0 in x:
+                            pole[x.index(0)][i] = 1
+                            turn = False
+                            break
+            if turn: # по диагонали 1
+                x = []
+                for i in range(0, 3):
+                    x.append(pole[i][i])
+                if sum(x) == 4 and 0 in x:
+                    if pole[x.index(0)][x.index(0)] == 0:
+                        pole[x.index(0)][x.index(0)] = 1
+                        turn = False
+            if turn: # по диагонали 2
+                x = []
+                for i in range(0, 3):
+                    x.append(pole[i][2 - i])
+                if sum(x) == 4 and 0 in x:
+                    if pole[x.index(0)][2 - x.index(0)] == 0:
+                        pole[x.index(0)][2 - x.index(0)] = 1
+                        turn = False
+            '''if turn:
+                while not isfull:
+                    isfull = True
+                    for i in pole:
+                        if 0 in i:
+                            isfull = False
+                            break
+                    field = [x for x in range(0, 9)]
+                    for i in pole:
+                        for j in pole:'''
+                            
     return pole
 
 
